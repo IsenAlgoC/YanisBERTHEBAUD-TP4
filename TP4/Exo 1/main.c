df1f1764e8881735613c8a88a251b638f4c69ed8
Yanis BERTHEBAUD <yanis.berthebaud@isen.yncrea.fr>
2020-10-07 14:09:57 +0200

    Exo 1 fini

---
+++ TP4/Exo 1/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+	#include "tp4.h"
+	HEURE HeureDebut = { 12, 30 };
+	HEURE HeureFin;
+	HEURE Duree = { 00, 45 };
+
+	HeureFin.heure = HeureDebut.heure + Duree.heure + (int)((HeureDebut.minute + Duree.minute)/60);
+	HeureFin.minute = (HeureDebut.minute + Duree.minute) % 60;
+	printf("HeureDebut = %d:%d\n", HeureDebut.heure, HeureDebut.minute);
+	printf("HeureFin = %d:%d\n", HeureFin.heure, HeureFin.minute);
+	printf("Duree = %d:%d\n", Duree.heure, Duree.minute);
+
+
+
+}
